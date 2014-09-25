298ee5733fa18520e0534bb059a002d3cad3dc9c
Marco Asbreuk <marco@itsasbreuk.nl>
2014-09-25 18:53:52 +0200

    removed typeof

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
 	idGenerator: require('./lib/idgenerator.js').idGenerator,
-	typeOf: require('./lib/typeof.js').typeOf,
 	later: require('./lib/timers.js').later,
 	async: require('./lib/timers.js').async
 };
