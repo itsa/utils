20875b398c7a0eb45b717437f91ea16606ea9c27
Marco Asbreuk <marco@itsasbreuk.nl>
2015-03-06 17:29:44 +0100

    removed finalization

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = {
 	idGenerator: require('./lib/idgenerator.js').idGenerator,
     later: require('./lib/timers.js').later,
-    later: require('./lib/timers.js').later,
-    async: require('./lib/timers.js').async,
     async: require('./lib/timers.js').async
 };
